a959a835e9996eed0d536d9863e33a4e5bc33e62
IUGAI <iugaialeksandrr@gmail.com>
2026-09-02 13:38:02 +0900

    Initial commit

---
+++ src/main/java/com/example/cgserver/CGserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CGserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CGserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgserver/CGserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CGserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
